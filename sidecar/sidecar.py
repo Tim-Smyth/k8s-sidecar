cb4d1bfffd561301c3c29f9280c725c49713a6a3
tim.smyth <tim.smyth@kiwigrid.com>
2018-04-30 14:09:02 +0200

    Missing logging issue

--- sidecar/sidecar.py
+++ sidecar/sidecar.py
@@ -14,7 +14,7 @@ def removeFile(folder, filename):
     if os.path.isfile(completeFile):
         os.remove(completeFile)
     else:
-        sys.stderr.write("Error: %s file not found" % completeFile)
+        print("Error: %s file not found" % completeFile)
 
 
 def watchForChanges(label, targetFolder):
@@ -23,16 +23,16 @@ def watchForChanges(label, targetFolder):
     for event in w.stream(v1.list_config_map_for_all_namespaces):
         if event['object'].metadata.labels is None:
             continue
-        sys.stdout.write("Working on configmap %s" % event['object'].metadata.name)
+        print("Working on configmap %s" % event['object'].metadata.name)
         if label in event['object'].metadata.labels.keys():
-            sys.stdout.write("Configmap with label found")
+            print("Configmap with label found")
             dataMap=event['object'].data
             if dataMap is None:
-                sys.stdout.write("Configmap does not have data.")
+                print("Configmap does not have data.")
                 continue
             eventType = event['type']
             for filename in dataMap.keys():
-                sys.stdout.write("File in configmap %s %s" % (filename, eventType))
+                print("File in configmap %s %s" % (filename, eventType))
                 if (eventType == "ADDED") or (eventType == "MODIFIED"):
                     writeTextToFile(targetFolder, filename, dataMap[filename])
                 else:
@@ -40,17 +40,17 @@ def watchForChanges(label, targetFolder):
 
 
 def main():
-    sys.stdout.write("Starting config map collector")
+    print("Starting config map collector")
     label = os.getenv('LABEL')
     if label is None:
-        sys.stderr.write("Should have added LABEL as environment variable! Exit")
+        print("Should have added LABEL as environment variable! Exit")
         return -1
     targetFolder = os.getenv('FOLDER')
     if targetFolder is None:
-        sys.stderr.write("Should have added FOLDER as environment variable! Exit")
+        print("Should have added FOLDER as environment variable! Exit")
         return -1
     config.load_incluster_config()
-    sys.stdout.write("Config for cluster api loaded...")
+    print("Config for cluster api loaded...")
     watchForChanges(label, targetFolder)
 
 
